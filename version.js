ac5b8d696f6e972a40cbe130c004826c3056dc5a
suchben <283226372@qq.com>
2018-09-08 19:23:37 +0800

    ..

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-export default const version = ["HEAD -> master","suchben","2018-09-08 19:06:21","f3ca7994e7526e5281222bed9d1c16920ab9eaef"]
+export default ["HEAD -> master, tag: xxx","suchben","2018-09-08 19:13:30","4febbe6231dde9e7ee841a116e4506231d16d4fb"]
