0740bdb1ba6aae0ab04007533a88bad1628213f0
suchben <283226372@qq.com>
2018-09-11 14:53:31 +0800

    rebase

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-export default ["HEAD -> master, origin/master, origin/HEAD","suchben","2018-09-08 19:24:17","0f2279c4031a6afeef90765a6506a67c65300f72"]
+export default ["HEAD -> master, origin/master, origin/HEAD","suchben","2018-09-10 21:09:08","0a512f2e1469cc9ae71cda8296787fdf7372cd06"]
